7fa1719f3e79c1ee3f5c280be04c595d71eb5056
DESKTOP-SSF64FD\ADM1N <daominh6879@gmail.com>
2021-08-02 00:32:33 +0700

    MobX

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,67 +1,28 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { Fragment, useEffect } from 'react';
 import { Container} from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
+import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
-  const [activities, setActivities] = useState<Activity[]>([])
-  const [selectiveAcivity, setSelectiveActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
+  const {activityStore} = useStore();
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(respone => {
-      setActivities(respone.data);
-    })
-  },[])
+    activityStore.loadActivities();
+  },[activityStore])
 
-  function handleSelectiveActivity(id : string) {
-    setSelectiveActivity(activities.find(x => x.id === id));
-  }
 
-  function handleCancelSelectieActivity() {
-    setSelectiveActivity(undefined);
-  }
-
-  function handleFormOpen(id?: string){
-    id? handleSelectiveActivity(id) : handleCancelSelectieActivity();
-    setEditMode(true);
-  }
-
-  function handleFormClose() {
-    setEditMode(false);
-  }
-
-  function handleCreateOrEditActivity(activity: Activity){
-    activity.id ? setActivities([...activities.filter(x => x.id !==activity.id), activity])
-    : setActivities([...activities, {...activity, id: uuid()}]);
-    setEditMode(false);
-    setSelectiveActivity(activity);
-  }
-
-  function handleDeleteActivity(id: string) {
-    setActivities([...activities.filter(x => x.id !== id)]);
-  }
+  if(activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
 
   return (
     <Fragment>
-      <NavBar openForm={handleFormOpen}/>
+      <NavBar/>
       <Container style={{marginTop: '7em' }}>
-        <ActivityDashboard 
-          activities= {activities}
-          selectedActivity={selectiveAcivity}
-          selectActivity={handleSelectiveActivity}
-          cancelSelectActivity={handleCancelSelectieActivity}
-          editMode={editMode}
-          openForm={handleFormOpen}
-          closeForm={handleFormClose}
-          createOrEditActivity={handleCreateOrEditActivity}
-          deleteActivity={handleDeleteActivity}
-        />
+        <ActivityDashboard/>
       </Container>
     </Fragment>
   );
 }
 
-export default App;
+export default observer(App);
